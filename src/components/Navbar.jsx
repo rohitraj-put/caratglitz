b40d9d5a92ad10daeb4812af58de96786fc8a6be
rohit rajput <rohitraj05072000@gmail.com>
2024-07-04 12:02:48 +0530

    remove wishlist number

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ import { PiChatsLight } from "react-icons/pi";
 
 
 
-function Navbar({ send, wishlist }) {
+function Navbar({ send }) {
     const [open, setOpen] = useState(false);
     const [isMenuOpen, setIsMenuOpen] = useState(false);
     const [query, setQuery] = useState('');
@@ -146,7 +146,7 @@ function Navbar({ send, wishlist }) {
                                 <svg xmlns="http://www.w3.org/2000/svg" width="20px" className="cursor-pointer fill-333 inline" viewBox="0 0 64 64">
                                     <path d="M45.5 4A18.53 18.53 0 0 0 32 9.86 18.5 18.5 0 0 0 0 22.5C0 40.92 29.71 59 31 59.71a2 2 0 0 0 2.06 0C34.29 59 64 40.92 64 22.5A18.52 18.52 0 0 0 45.5 4ZM32 55.64C26.83 52.34 4 36.92 4 22.5a14.5 14.5 0 0 1 26.36-8.33 2 2 0 0 0 3.27 0A14.5 14.5 0 0 1 60 22.5c0 14.41-22.83 29.83-28 33.14Z" data-original="#000000"></path>
                                 </svg>
-                                <span className="absolute left-auto -ml-1 top-0 rounded-full bg-red-500 px-1 py-0 text-xs text-white">{wishlist.length}</span>
+                                <span className="absolute left-auto -ml-1 top-0 rounded-full bg-red-500 px-1 py-0 text-xs text-white"></span>
                             </span>
                             <span className="relative">
                                 <Link to="/cartlist">
